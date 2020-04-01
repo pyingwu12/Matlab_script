85735e5fb939ead3c2727ba47a0d4d6461760c75
pyingwu12 <pyingwu12@gmail.com>
2020-04-01 12:54:41 +0900

    zh_model_cmpo_en.m

---
+++ zh_model_cmpo_en.m
@@ -0,0 +1,66 @@
+close all
+clear
+%---setting
+expri='ens03';  member=1;
+%year='2007'; mon='06'; date='01';
+year='2018'; mon='06'; date='22';  hr=0:10; minu=00; 
+dirmem='pert'; infilenam='wrfout';  dom='01';  
+%scheme='Gaddard';
+scheme='WSM6';
+
+%indir=['/HDD001/expri_ens200323/',expri];
+indir=['/HDD003/pwin/Experiments/expri_ens200323/',expri];
+outdir=['/HDD001/Figures/ens200323/',expri];
+titnam='Zh composite';   fignam=[expri,'_zh-model_'];
+
+load('colormap/colormap_zh.mat')
+cmap=colormap_zh; cmap(1,:)=[1 1 1];
+cmap2=cmap*255;cmap2(:,4)=zeros(1,size(cmap2,1))+255;
+L=[0 2 6 10 15 20 25 30 35 40 45 50 55 60 65 70];
+%---
+%%
+for ti=hr   
+  for tmi=minu    
+    for mi=member
+      %ti=hr; mi=minu;
+      %---set filename---
+      s_hr=num2str(ti,'%.2d');  % start time string
+      s_min=num2str(tmi,'%.2d');
+      nen=num2str(mi,'%.2d');
+      infile=[indir,'/',dirmem,nen,'/',infilenam,'_d',dom,'_',year,'-',mon,'-',date,'_',s_hr,':',s_min,':00'];
+      zh_max=cal_zh_cmpo(infile,scheme);  
+      hgt = ncread(infile,'HGT');      
+%     end
+%   end
+% end
+      
+      %---plot---
+      plotvar=zh_max';   %plotvar(plotvar<=0)=NaN;
+      pmin=double(min(min(plotvar)));   if pmin<L(1); L2=[pmin,L]; else; L2=[L(1) L]; end
+       %
+      hf=figure('position',[-900 200 830 600]);
+    
+      [c, hp]=contourf(plotvar,L2,'linestyle','none');  
+      set(gca,'fontsize',16,'LineWidth',1.2)
+      
+      if (max(max(hgt))~=0)
+        hold on; contour(hgt',[100 500 900],'color',[0.55 0.55 0.55],'linestyle','--'); 
+      end
+      tit=[expri,'  mem',nen,'  ',titnam,'  ',s_hr,s_min,' UTC'];     
+      title(tit,'fontsize',17)
+%---
+      L1=((1:length(L))*(diff(caxis)/(length(L)+1)))+min(caxis());
+      h=colorbar('YTick',L1,'YTickLabel',L,'fontsize',13,'LineWidth',1);
+      colormap(cmap)
+   
+      drawnow;
+      hFills = hp.FacePrims;  % array of matlab.graphics.primitive.world.TriangleStrip objects
+      for idx = 1 : numel(hFills)
+         hFills(idx).ColorData=uint8(cmap2(idx,:)');
+      end   
+%---    
+      outfile=[outdir,'/',fignam,mon,date,'_',s_hr,s_min,'_m',nen];
+      print(hf,'-dpng',[outfile,'.png']) 
+   end
+ end
+end
