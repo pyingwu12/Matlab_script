b121c13be92dc48837ac41d9799d6f3176dd5709
WU_PY <WU_PY@DESKTOP-AF1SNSA>
2020-11-03 15:37:55 +0900

    2020/11/03 at windows modified find_convection

--- a_find_convective.m
+++ a_find_convective.m
@@ -1,13 +1,14 @@
-close all
-clear;   ccc=':';
+% close all
+clear;   ccc='%3A';
 %---setting
-expri='TWIN003B';  s_date='23'; hr=3; minu=00; 
+expri='TWIN003B';  s_date='23'; hr=1; minu=00; 
 %---
 year='2018'; mon='06'; 
 infilenam='wrfout';  dom='01';  grids=1; %grid_spacing(km)
 scheme='WSM6';
 %---
-indir=['/mnt/HDD008/pwin/Experiments/expri_twin/',expri]; outdir=['/mnt/e/figures/expri_twin/',expri(1:7)];
+%indir=['/mnt/HDD008/pwin/Experiments/expri_twin/',expri]; outdir=['/mnt/e/figures/expri_twin/',expri(1:7)];
+indir=['e:/wrfout/expri_twin/',expri]; outdir=['/mnt/e/figures/expri_twin/',expri(1:7)];
 %---
 titnam='Zh composite';   fignam=[expri,'_zh_'];
 %
@@ -23,13 +24,19 @@ for ti=hr
     zh_max=cal_zh_cmpo(infile,scheme);         
     hgt = ncread(infile,'HGT');  
     %
-    
     BW=zeros(size(zh_max));
     BW(zh_max>2)=1;
     CC = bwconncomp(BW,8);
     
+    L = labelmatrix(CC);
+%     imshow(label2rgb(L,'jet','k','shuffle'));
+    A=L; A(L~=0)=1;
+    figure
+    contour(A',[1 1])
     
-
+    for i=1:size(CC.PixelIdxList,2)
+     a(i)=size(CC.PixelIdxList{i},1);
+    end
    
   end
 end
